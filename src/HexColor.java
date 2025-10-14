0f8f1919ff6af136c2e385cb7b09dfcfd3fb745c
Anna Huang <76632032+sayoku@users.noreply.github.com>
2025-10-14 18:32:52 -0400

    Fleshing out HexColor Secondary methods

--- src/HexColor.java
+++ src/HexColor.java
@@ -3,12 +3,20 @@
 /**
  * {@code HexColorKernel} enhanced with secondary methods.
  */
-public interface HexColor {
+public interface HexColor extends HexColorKernel {
+
+    /**
+     * Regular format for validating Hex Color Code. Accepts 6-digit and 3-digit
+     * formats. I am not sure if this should be in the kernel or the
+     * implementation!!
+     */
+    String FORMAT_HEX = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
 
     /**
      * Extract red component.
      *
      * @return red value as a string
+     * @ensures getRed = this.substring[1, 2] where this = "#RRGGBB"
      */
     String getRed();
 
@@ -16,6 +24,7 @@ public interface HexColor {
      * Extract green component.
      *
      * @return green value as a string
+     * @ensures getGreen = this.substring[3, 4] where this = "#RRGGBB"
      */
     String getGreen();
 
@@ -23,6 +32,7 @@ public interface HexColor {
      * Extract green component.
      *
      * @return green value as a string
+     * @ensures getGreen = this.substring[5, 6] where this = "#RRGGBB"
      */
     String getBlue();
 
@@ -35,48 +45,76 @@ public interface HexColor {
      *            green component (0-255)
      * @param blue
      *            blue component (0-255)
-     *
+     * @replaces this
+     * @requires 0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255.
+     * @ensures this is "#" * the 2-digit hexadecimal representations of red,
+     *          blue, and green
      */
     void setRGB(int red, int green, int blue);
 
     /**
-     * Check to make sure hex code format is correct.
+     * Checks whether a given hex color code fits the valid format.
      *
-     * @param hex
+     * @param hexValue
      *            the hex value to check
-     * @return true if format is valid, false otherwise
+     * @return true iff format is a valid hex color code, false otherwise
+     * @ensures isValidHex matches the format specified by FORMAT_HEX
      */
-    boolean isValidHex(String hex);
+    boolean isValidHex(String hexValue);
 
     /**
-     * return lighter version.
+     * Returns a lighter version of this.
      *
      * @param factor
-     * @return lighter
+     *            the lightening factor 0.0 to 1.0
+     * @return a lighter color
+     * @requires 0.0 <= factor <= 1.0
+     * @ensures lighten is a valid hex color code where each component is moved
+     *          towards 255 by factor
      */
     HexColor lighten(double factor);
 
     /**
-     * return darker version.
+     * Returns a darker version of this.
      *
      * @param factor
-     * @return darker
+     *            the darkening factor 0.0 to 1.0
+     * @return a darker color
+     * @requires 0.0 <= factor <= 1.0
+     * @ensures darker is a valid hex color code where each component is moved
+     *          towards 0 by factor
      */
     HexColor darken(double factor);
 
     /**
-     * calulcate brightness.
+     * Returns the luminance of this.
      *
-     * @return luminance
+     * @return the luminance of the color
+     * @ensures luminance = (0.2126 * R) + (0.7152 * G) + (0.0722 * B) where R,
+     *          G, and B are the corresponding substring of the Hex Color Code
+     *          this
      */
     double getLuminance();
 
     /**
-     * return complementary color.
+     * Returns the complementary color of this (opposite on the color wheel).
      *
-     * @return complement
+     * @return the complement of the color
+     * @ensures complement = "#" * (255 - R) * (255 - G) * (255 - B), where R,
+     *          G, and B are the corresponding substring of the Hex Color Code
+     *          this
+     *
      */
     HexColor complement();
+
+    /**
+     * Returns a string representation of the hex color in standard format
+     * "#RRGGBB".
+     *
+     * @return the string representation of the color
+     * @ensures toString = this in uppercase format
+     *
+     */
 
     @Override
     String toString();
