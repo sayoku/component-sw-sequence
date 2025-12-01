b5fd219a61f121eca006284a29588dd079af3375
Anna Huang <76632032+sayoku@users.noreply.github.com>
2025-12-01 08:18:41 -0500

    Simple Palette Generator use case, given a primary color, find the complement, light, and dark versions

--- src/PaletteGenerator.java
+++ src/PaletteGenerator.java
@@ -3,14 +3,26 @@ import components.HexColorString;
 
 public class PaletteGenerator {
 
-    private HexColor primary;
+    private HexColor color;
 
-    public PaletteGenerator(String primary) {
-        this.primary = new HexColorString();
+    // Palette Generator uses the HexColor STRING constructor
+    public PaletteGenerator(String colory) {
+        this.color = new HexColorString();
     }
 
     public static void main(String[] args) {
 
+        PaletteGenerator theme = new PaletteGenerator("#6495ED");
+
+        HexColor primary = theme.color;
+        HexColor accent = primary.complement();
+        HexColor light = primary.lighten(0.2);
+        HexColor dark = primary.darken(0.3);
+
+        System.out.println("Primary: " + primary);
+        System.out.println("Accent: " + accent);
+        System.out.println("Light: " + light);
+        System.out.println("Dark: " + dark);
     }
 
 }
